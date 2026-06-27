12bba2ae92547eb6efb42b66473088ca886d727d
humayranuha <nuhahumayra24@gmail.com>
2026-06-27 23:48:36 +0600

    feat: numerical types in python along with random module

---
+++ numbers.py
@@ -0,0 +1,20 @@
+import random
+
+x = 5
+y = 3.14
+z = 1j # complex number: the imaginary part is expressed with 'j'
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+a = float(x)
+b = int(y)
+c = complex(x)
+
+print(a, b, c)
+print(type(a))
+print(type(b))
+print(type(c))
+
+print(random.randrange(1, 6))
