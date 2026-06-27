3cde9a6263e5abfda305f745367e8d5c244bf165
humayranuha <nuhahumayra24@gmail.com>
2026-06-27 20:05:08 +0600

    feat: values and assigning varieties

--- variables.py
+++ variables.py
@@ -8,3 +8,12 @@ z = float(1)
 
 print(a, x, y, z)
 print(type(a), type(x), type(y), type(z))
+
+# assigning values to multiple variables
+p, q, r = "psg", "quinton", "rial"
+
+# assigning same value to multiple variables
+t = u = v = "orange"
+
+print(p, q, r)
+print(t, u, v)
