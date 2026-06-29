a01bbc090d255742089f8bdc8229ac55b7b7434e
humayranuha <nuhahumayra24@gmail.com>
2026-06-29 11:10:58 +0600

    feat: arithmetic operators

---
+++ arithmetic.py
@@ -0,0 +1,9 @@
+x = 15
+y = 4
+print(x + y)
+print(x - y)
+print(x * y)
+print(x / y)
+print(x % y)
+print(x ** y) # exponentiation
+print(x // y) # floor division - returns an integer(rounds down to the nearest integer)
