568816d2162095c586d4dff61cf869b2e9b447a2
humayranuha <nuhahumayra24@gmail.com>
2026-06-29 14:54:15 +0600

    feat: identity operator in python

---
+++ identityOperator.py
@@ -0,0 +1,8 @@
+x = ["apple", "banana"]
+y = ["apple", "banana"]
+z = x
+
+print(x is y) # Checks if both variables point to the same object in memory
+print(x == y) # Checks if the values of both variables are equa
+print(x is not y)
+print(x is z)
