22ab2fb016f7efaedd878315889ee24dbe82e583
humayranuha <nuhahumayra24@gmail.com>
2026-06-29 20:50:23 +0600

    feat: list type in python

---
+++ list.py
@@ -0,0 +1,7 @@
+# list is a collection which is ordered and changeable. Allows duplicate members. 
+list1 = ["apple", "banana", "cherry"]
+list2 = list(("apple", "banana")) # double round-brackets
+list3 = ["apple", "banana", "cherry", "apple"] # duplicate values allowed
+print(list1)
+print(type(list2))
+print(len(list3))
