11831092d32330fd81fff3db82820e58f38da3b0
humayranuha <nuhahumayra24@gmail.com>
2026-06-29 20:56:33 +0600

    feat: several ways to access list items

--- list.py
+++ list.py
@@ -5,3 +5,10 @@ list3 = ["apple", "banana", "cherry", "apple"] # duplicate values allowed
 print(list1)
 print(type(list2))
 print(len(list3))
+
+# Accessing list items
+print(list1[0])
+print(list1[-1])
+print(list3[1:3])
+print(list3[-3:-1])
+
